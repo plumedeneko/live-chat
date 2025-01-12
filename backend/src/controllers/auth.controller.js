3c05e5945ca331037771002c60f730762cc58360
Kunal Rishi <kunalrishi22@gmail.com>
2025-01-11 23:48:19 -0500

    First

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const signup = (req, res) => {
+    try {
+        // hash password
+    } catch (error) {
+        
+    }
+}
+
+export const login = (req, res) => {
+    res.send("login route");
+}
+
+export const logout = (req, res) => {
+    res.send("logout route");
+}
