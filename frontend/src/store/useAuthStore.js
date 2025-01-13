f90996dac4900cf3d05afc5c7337f9405c26df5c
Kunal Rishi <kunalrishi22@gmail.com>
2025-01-13 18:43:28 -0500

    formatting

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,25 +1,27 @@
-import { create } from "zustand";
-import { axiosInstance } from "../lib/axios.js";
+import {create} from "zustand";
+import {axiosInstance} from "../lib/axios.js";
 
 export const useAuthStore = create((set) => ({
-  authUser: null,
-  isSigningUp: false,
-  isLoggingIn: false,
-  isUpdatingProfile: false,
+    authUser: null,
+    isSigningUp: false,
+    isLoggingIn: false,
+    isUpdatingProfile: false,
 
-  isCheckingAuth: true,
+    isCheckingAuth: true,
 
-  checkAuth: async () => {
-    try {
-      const res = await axiosInstance.get("/auth/check");
-      set({ authUser: res.data });
-    } catch (error) {
-      console.log("Error in checkAuth:", error);
-      set({ authUser: null });
-    } finally {
-      set({ isCheckingAuth: false });
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get("/auth/check");
+            set({authUser: res.data});
+        } catch (error) {
+            console.log("Error in checkAuth:", error);
+            set({authUser: null});
+        } finally {
+            set({isCheckingAuth: false});
+        }
+    },
+
+    signup: async (data) => {
+
     }
-  },
-
-  signup: async (data) => {},
-}));
+}))
