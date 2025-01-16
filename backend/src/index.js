ecb30d0431c57432a0906bbf553e4fc677c243fd
Kunal Rishi <kunalrishi22@gmail.com>
2025-01-15 20:54:50 -0500

    websocket implementation start

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,9 @@ import cors from "cors";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json({ limit: '10mb' }));
@@ -21,7 +21,7 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectDB();
 });
